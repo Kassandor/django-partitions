5e1c93a42e779960f09feb1530d1a721c94a831e
Kassandor <webmasterdracarys@gmail.com>
2025-11-28 15:28:15 +0300

    Создана партицированная таблица Orders через pgsql, далее подключена к модели django orm

--- partitions/models.py
+++ partitions/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Order(models.Model):
+    pk = models.CompositePrimaryKey('id', 'created_at')
+    id = models.IntegerField()
+    user = models.ForeignKey(User, verbose_name="Пользователь", on_delete=models.CASCADE)
+    created_at = models.DateField()
+    status = models.CharField(verbose_name='Статус', max_length=20)
+
+    class Meta:
+        db_table = 'django_orders'
